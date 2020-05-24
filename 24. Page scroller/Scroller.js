32c569704f0f2b068d755582fa422ca228710190
Szczepan <s.szablej1@gmail.com>
2020-05-25 00:34:41 +0200

    Add navigation to the scroller feature

--- 24. Page scroller/Scroller.js
+++ 24. Page scroller/Scroller.js
@@ -8,6 +8,7 @@ class Scroller {
     );
     this.currentSectionIndex = Math.max(currentSectionIndex, 0);
     this.isThrottled = false;
+    this.drawNavigation();
   }
 
   isScrolledIntoView(el) {
@@ -42,9 +43,44 @@ class Scroller {
   };
 
   scrollToCurrentSection = () => {
+    this.selectActiveNavItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
+  };
+
+  drawNavigation = () => {
+    this.navigationContainer = document.createElement("aside");
+    this.navigationContainer.classList.add("scroller__navigation");
+    const list = document.createElement("ul");
+
+    this.sections.forEach((section, index) => {
+      const listItem = document.createElement("li");
+      listItem.addEventListener("click", () => {
+        this.currentSectionIndex = index;
+
+        this.scrollToCurrentSection();
+      });
+      list.appendChild(listItem);
+    });
+
+    this.navigationContainer.appendChild(list);
+    document.body.appendChild(this.navigationContainer);
+    this.selectActiveNavItem();
+  };
+
+  selectActiveNavItem = () => {
+    if (this.navigationContainer) {
+      const navigationItems = this.navigationContainer.querySelectorAll("li");
+
+      navigationItems.forEach((item, index) => {
+        if (index === this.currentSectionIndex) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    }
   };
 }
