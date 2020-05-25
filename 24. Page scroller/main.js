a526db2947757636d5cbde637f0122473618a9e5
Szczepan <s.szablej1@gmail.com>
2020-05-25 02:07:47 +0200

    Add support for keyboard

--- 24. Page scroller/main.js
+++ 24. Page scroller/main.js
@@ -3,4 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("mousewheel", scroller.listenScroll);
   document.addEventListener("swipeUp", () => scroller.scroll(1));
   document.addEventListener("swipeDown", () => scroller.scroll(-1));
+  document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+      case 40:
+        return scroller.scroll(1);
+      case 38:
+        return scroller.scroll(-1);
+      default:
+        return;
+    }
+  });
 });
