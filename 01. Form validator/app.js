8d17bf79463d3ab70c728ae4720c84286be2a88b
Szczepan <s.szablej1@gmail.com>
2020-06-02 22:06:48 +0200

    Add checkEmail, checkLength and checkPasswordMatch functions

--- 01. Form validator/app.js
+++ 01. Form validator/app.js
@@ -19,9 +19,13 @@ function showSuccess(input) {
 }
 
 //Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 //Check required fields
@@ -35,6 +39,25 @@ function checkRequired(inputArr) {
   });
 }
 
+//Check Length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must at least ${min} chars`);
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be shorter than ${max} chars`
+    );
+  } else showSuccess(input);
+}
+
+//Check password
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
 //Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -45,4 +68,8 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkPasswordMatch(password, password2);
+  checkEmail(email);
 });
