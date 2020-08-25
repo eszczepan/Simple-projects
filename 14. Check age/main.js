49129d98bc570b62cc52dc168a13c297b44b1a02
Szczepan <s.szablej1@gmail.com>
2020-08-25 21:42:24 +0200

    :hammer: Refactor code

--- 14. Check age/main.js
+++ 14. Check age/main.js
@@ -1,12 +1,22 @@
+const button = document.querySelector(".button");
+const inputName = document.querySelector(".input-name");
+const inputAge = document.querySelector(".input-age");
+
 const user = (name, age) => {
-  let userName = name;
-  let userAge = age;
+  document.body.textContent = `Hey ${name}. ${
+    age >= 18
+      ? `You are ${age}, so you are allowed to buy a beer`
+      : "You are under 18, so you cannot buy a beer"
+  }`;
+};
 
-  function showMessage() {
-    document.body.textContent = `Hey ${userName}, ${userAge >= 18 ? 'You can buy a beer' : 'You cannot buy a beer'}`
+const handleButtonClick = (e) => {
+  e.preventDefault();
+  const name = inputName.value;
+  const age = inputAge.value;
+  if (name && age) {
+    return user(name, age);
   }
-  return showMessage
-}
+};
 
-const szcz = user('Szczeps', 15);
-szcz()
+button.addEventListener("click", handleButtonClick);
