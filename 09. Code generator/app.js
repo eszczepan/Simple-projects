9ab83f06c327c05f80d4bd2e28798722daf84ac0
Szczepan <s.szablej1@gmail.com>
2020-08-25 21:21:38 +0200

    :hammer: Refactor code

--- 09. Code generator/app.js
+++ 09. Code generator/app.js
@@ -1,15 +1,18 @@
-const btn = document.querySelector('button');
-const chars = 'ABCDEFGHIJMK0123456789'
+const btn = document.querySelector("button");
+const chars =
+  "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
 
 function generateCode() {
-  for (let i = 0; i < 10; i++) {
-    const h1 = document.createElement('h1');
-    for (let i = 0; i < 10; i++) {
-      const index = Math.floor(Math.random() * chars.length);
-      h1.textContent += chars[index]
-    }
-    document.body.appendChild(h1)
+  const oldh1 = document.querySelector("h1");
+  if (oldh1) {
+    oldh1.remove();
   }
+  const h1 = document.createElement("h1");
+  for (let i = 0; i < 13; i++) {
+    const index = Math.floor(Math.random() * chars.length);
+    h1.textContent += chars[index];
+  }
+  document.body.appendChild(h1);
 }
 
-btn.addEventListener('click', generateCode)
+btn.addEventListener("click", generateCode);
